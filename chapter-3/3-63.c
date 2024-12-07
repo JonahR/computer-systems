34b4422084d61f0e39193b85303d422fbf22dbe9
Jonah Rueb <ruebjonah@gmail.com>
2024-12-07 13:08:24 -0500

    solution to 3.63

---
+++ chapter-3/3-63.c
@@ -0,0 +1,9 @@
+#define E1(n) (2*n + 1)
+#define E2(n) (12 * n)
+int sum_col(int n, int A[E1(n)][E2(n)], int j) {
+	int i;
+	int result = 0;
+	for (i = 0; i < E1(n); i++)
+		result += A[i][j];
+	return result;
+}
