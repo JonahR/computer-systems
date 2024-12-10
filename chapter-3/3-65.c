bd9a9ff429f050b9ebc1ad36b23e487f316e1515
Jonah Rueb <ruebjonah@gmail.com>
2024-12-09 20:52:47 -0500

    solution for 3.65.c

---
+++ chapter-3/3-65.c
@@ -0,0 +1,18 @@
+typedef struct {
+	short x[A][B];
+	int y;
+} str1;
+
+typedef struct {
+	char array[B];
+	int t;
+	short s[B];
+	int u;
+} str2;
+
+void setVal(str1 *p, str2 *q) {
+	int v1 = q ->t;
+	int v2 = q->u;
+	p->y = v1+v2;
+}
+// A = 6 B = 2
