0d46fd98bf5f87b7559bce4b4d60f49a094a37a0
Jonah Rueb <ruebjonah@gmail.com>
2024-12-10 22:13:11 -0500

    Solution for 3.67

---
+++ chapter-3/3.67.c
@@ -0,0 +1,22 @@
+// e1.p: 0
+// e1.y: 4
+// e2.x: 0
+// e2.next: 4
+
+// The struct would require 8 total bytes
+
+union ele {
+	struct {
+		int *p;
+		int y;
+	} e1;
+	struct {
+		int x;
+		union ele *next;
+	} e2;
+};
+
+void proc (union ele *up)
+{
+	up->next->p = *((up->next).p) - up->x;
+}
