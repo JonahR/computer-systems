532a2f7e87a6be23379da2976fc99e4d5e01ca2b
Jonah Rueb <ruebjonah@gmail.com>
2025-04-17 22:02:49 -0400

    Solves 8.5

---
+++ chapter-8/8-05.c
@@ -0,0 +1,16 @@
+#include "csapp.h"
+
+unsigned int snooze(unsigned int secs) {
+	int remaining;
+
+	remaining = sleep(secs);
+	printf("Slept for %d of %d secs\n", secs - remaining, secs);
+
+	return(0);
+}
+
+
+int main() {
+	snooze(3);
+}
+
